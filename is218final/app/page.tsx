9d088b799736f89a70153926989c85bbc4d74098
Sally Chik <sc399@njit.edu>
2023-12-12 23:38:52 -0500

    Adding final hero image, styling final hero image

--- is218final/app/page.tsx
+++ is218final/app/page.tsx
@@ -6,24 +6,28 @@ export default function Home() {
   return (
     <div className='min-w-full min-h-full'>
       <Navigation/>
-      <div className='align-middle justify-center'style={{
-        // backgroundImage: `url("/hero.png")`,
-        // backgroundSize: 'cover',
-        backgroundColor: 'saddlebrown',
-        height: '30vw'
-      }}>
-      <div className='flex flex-col overflow-visible w-4/6 px-16 py-32'>
-        <h1 className=' text-3xl'>
-          INDULGE IN CHILEAN FLAVORS
-        </h1>
-        <h3>
-          TRY OUR SANTIAGO CATCH THIS MONTH ONLY
-GET 10% OFF WHEN YOU RESERVE A TABLE
-        </h3>
-        <Button className=' w-1/4'>
-          Reserve
-        </Button>
-      </div>
+      <div className='relative'>
+        <div className='align-middle justify-center'style={{
+          backgroundImage: `url("/hero.svg")`,
+          backgroundSize: 'cover',
+          backgroundPositionY: '90%',
+          paddingBottom: '600px',
+          width: '100%',
+          opacity: '.6',
+          zIndex: '-1'
+        }}/>
+        <div className='flex flex-col overflow-visible w-4/6 px-16 py-16 z-10 absolute top-52'>
+          <h1 className=' text-3xl'>
+            INDULGE IN CHILEAN FLAVORS
+          </h1>
+          <h3>
+            TRY OUR SANTIAGO CATCH THIS MONTH ONLY
+  GET 10% OFF WHEN YOU RESERVE A TABLE
+          </h3>
+          <Button className=' w-1/4'>
+            Reserve
+          </Button>
+        </div>
       </div>
     </div>
   )
