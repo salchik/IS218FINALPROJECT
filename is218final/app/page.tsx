ae42032d4a628316523e560373d0d387eb1f8df0
Sally Chik <sc399@njit.edu>
2023-12-13 17:16:00 -0500

    Adding about text and styling container div flexes

--- is218final/app/page.tsx
+++ is218final/app/page.tsx
@@ -5,9 +5,9 @@ import { Button } from '@nextui-org/react';
 export default function Home() {
   return (
     <div className='min-w-full min-h-full'>
-      <RestNav/>
+      <RestNav />
       <div className='relative'>
-        <div className='align-middle justify-center'style={{
+        <div className='align-middle justify-center' style={{
           backgroundImage: `url("/hero.svg")`,
           backgroundSize: 'cover',
           backgroundPositionY: '90%',
@@ -15,51 +15,59 @@ export default function Home() {
           width: '100%',
           opacity: '.3',
           zIndex: '-1'
-        }}/>
+        }} />
         <div className='flex flex-col overflow-visible w-11/12 px-32 py-16 z-10 absolute top-16'>
           <h1 className='text-8xl font-playfair'>
             INDULGE IN CHILEAN FLAVORS
           </h1>
           <h3 className='font-roboto font-light text-lg w-5/12 my-10'>
             TRY OUR SANTIAGO CATCH THIS MONTH ONLY
-  GET 10% OFF WHEN YOU RESERVE A TABLE
+            GET 10% OFF WHEN YOU RESERVE A TABLE
           </h3>
           <Button className=' w-4/12 text-2xl font-playfair bg-buttonRed py-3 rounded'>
-          {/* #A52323 */}
+            {/* #A52323 */}
             Reserve
           </Button>
         </div>
       </div>
-      <div className='w-full py-5'/>
-      <div className=' w-full bg-white py-40'>
-        <div className='text-black'>
+      <div className='w-full py-5' />
+      <div className='w-full bg-white py-40 flex justify-center'>
+        <div className='text-black w-full flex flex-col text-center'>
           <h3>
             words of rapture
           </h3>
           <h1>
-          rhapsodies from our beloved patrons
+            rhapsodies from our beloved patrons
           </h1>
-        </div>
-        <div>
-          <ul>
-            <li>
+          <ul className='inline-flex justify-center'>
+            <li className='w-1/4'>
               <div>
                 <h4>
-                “SANCTUARY WHERE LOVE AND SEAFOOD COMBINE.”
+                  “SANCTUARY WHERE LOVE AND SEAFOOD COMBINE.”
                 </h4>
                 <p>
                   PIERRE AND CHERYL
                 </p>
               </div>
             </li>
-            <li>
+            <li className='w-1/4'>
               <div>
-                  
+                <h4>
+                  “SANCTUARY WHERE LOVE AND SEAFOOD COMBINE.”
+                </h4>
+                <p>
+                  PIERRE AND CHERYL
+                </p>
               </div>
             </li>
-            <li>
+            <li className='w-1/4'>
               <div>
-                
+                <h4>
+                  “SANCTUARY WHERE LOVE AND SEAFOOD COMBINE.”
+                </h4>
+                <p>
+                  PIERRE AND CHERYL
+                </p>
               </div>
             </li>
           </ul>
