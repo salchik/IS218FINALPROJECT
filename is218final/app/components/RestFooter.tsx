7142cb0c0bb9128af43dcb97578b26226634fe3f
Sally Chik <sc399@njit.edu>
2023-12-14 08:58:56 -0500

    Styled facebook and instagram links

--- is218final/app/components/RestFooter.tsx
+++ is218final/app/components/RestFooter.tsx
@@ -25,12 +25,14 @@ export default function RestFooter() {
               </NavbarItem>
             </NavbarContent>
           </Navbar>
-          <Link href='www.facebook.com'>
-            <Image src='/facebookIcon.svg' width={50}/>
-          </Link>
-          <Link href='www.instagram.com'>
-            <Image src='/instagramIcon.svg' width={50}/>
-          </Link>
+          <div className='flex justify-center gap-10 pb-10'>
+            <Link href='www.facebook.com'>
+                <Image src='/facebookIcon.svg' width={50}/>
+            </Link>
+            <Link href='www.instagram.com'>
+                <Image src='/instagramIcon.svg' width={50}/>
+            </Link>
+          </div>
       </header>
       )
 }
