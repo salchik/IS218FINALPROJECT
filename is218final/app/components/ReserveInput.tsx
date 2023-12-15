6be5a43ccf6e0cdae40a24b7a5f03f68b4fac0f0
Sally Chik <sc399@njit.edu>
2023-12-15 00:25:23 -0500

    Changed api file to js

--- is218final/app/components/ReserveInput.tsx
+++ is218final/app/components/ReserveInput.tsx
@@ -3,23 +3,38 @@ import { Button, Input } from '@nextui-org/react'
 import { useRef } from 'react'
 
 export default function ReserveInput() {
+    const testData = {
+        id: '1'
+    }
     const inputRef = useRef(null)
     const subscribeUser = async (e: any) => {
         e.preventDefault()
 
-        const res = fetch('/api')
-    } 
+        const res = fetch('/api',
+            {
+                body: JSON.stringify({
+                    //inputRef.current.value
+                    email: testData
+                }),
+
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+
+                method: 'POST',
+            });
+    };
     return (
         <div className="w-2/4 mt-10">
-        <div className="flex flex-row gap-3">
-            <Input className="w-6/12" type="text" label={'Your First Name'} />
-            <Input className="w-6/12" type="text" label={'Your Last Name'} />
+            <div className="flex flex-row gap-3">
+                <Input className="w-6/12" type="text" label={'Your First Name'} />
+                <Input className="w-6/12" type="text" label={'Your Last Name'} />
+            </div>
+            <Input className="w-full mt-10" type="email" label={'Your Email'} />
+            <Input className="w-full mt-10" type="text" label={'Amount of Guests'} />
+            <Button color='secondary' className=' w-full text-2xl my-14 font-roboto font-light bg-black py-7 rounded-lg'>
+                RESERVE
+            </Button>
         </div>
-        <Input className="w-full mt-10" type="email" label={'Your Email'} />
-        <Input className="w-full mt-10" type="text" label={'Amount of Guests'} />
-        <Button color='secondary' className=' w-full text-2xl my-14 font-roboto font-light bg-black py-7 rounded-lg'>
-            RESERVE
-        </Button>
-    </div>
-      )
+    )
 }
