036e94726fff76c2ac28d2f29a7da78b273f400e
Sally Chik <sc399@njit.edu>
2023-12-14 19:20:15 -0500

    Adding reservation and about folders and pages

---
+++ is218final/app/about/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@nextui-org/react";
+
+export default function About() {
+    return (
+      <div className="flex min-h-screen">
+        <div>
+          <Button color='danger'>About !!!</Button>
+        </div>
+      </div>
+    )
+  }
---
+++ is218final/app/reservation/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@nextui-org/react";
+
+export default function Reservation() {
+    return (
+      <div className="flex min-h-screen">
+        <div>
+          <Button color='danger'>Reservation !!!</Button>
+        </div>
+      </div>
+    )
+  }
