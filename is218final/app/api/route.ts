921c85fd6ede3fb92bb67175b70dc163c65ef198
Sally Chik <sc399@njit.edu>
2023-12-15 01:43:07 -0500

    Adding mailchimp call with placeholder data

--- is218final/app/api/route.ts
+++ is218final/app/api/route.ts
@@ -1,25 +1,26 @@
 
-export default async (req, res) => {
-    const { email } = req.body;
-  
-    console.log({ email });
-  
-    if (!email) {
-      return res.status(400).json({ error: 'Email is required' });
+export async function POST(req: Request, res: Response){
+    const buffer = await req.arrayBuffer()
+    const inputEmail = new TextDecoder('utf-8').decode(buffer)
+    
+    if (!inputEmail) {
+      res = Response.json({ error: 'Email is required' })
+      return res;
     }
+    const emailJSON = JSON.parse(inputEmail)
+    const emailString = emailJSON['input_email']
   
     try {
       const AUDIENCE_ID = process.env.MAILCHIMP_AUDIENCE_ID;
       const API_KEY = process.env.MAILCHIMP_API_KEY;
       const DATACENTER = process.env.MAILCHIMP_API_SERVER;
       const data = {
-        email_address: email,
+        email_address: emailString,
         status: 'subscribed',
       };
   
       const response = await fetch(
         `https://${DATACENTER}.api.mailchimp.com/3.0/lists/${AUDIENCE_ID}/members`,
-  
         {
           body: JSON.stringify(data),
           headers: {
@@ -29,16 +30,11 @@ export default async (req, res) => {
           method: 'POST',
         }
       );
-  
-      if (response.status >= 400) {
-        return res.status(400).json({
-          error: `There was an error subscribing to the newsletter.
-          Hit me up peter@peterlunch.com and I'll add you the old fashioned way :(.`,
-        });
-      }
-  
-      return res.status(201).json({ error: '' });
+      console.log(data)
+      res = Response.json('Everything went through!')
+      return res;
     } catch (error) {
-      return res.status(500).json({ error: error.message || error.toString() });
+      res = Response.json({ error: 'Something went wrong, not email!' })
+      return res;
     }
   };
--- is218final/app/components/ReserveInput.tsx
+++ is218final/app/components/ReserveInput.tsx
@@ -3,9 +3,7 @@ import { Button, Input } from '@nextui-org/react'
 import { useRef } from 'react'
 
 export default function ReserveInput() {
-    const testData = {
-        id: '1'
-    }
+    const testData = 'example@gmail.com'
     const inputRef = useRef(null)
     const subscribeUser = async (e: any) => {
         e.preventDefault()
@@ -14,7 +12,7 @@ export default function ReserveInput() {
             {
                 body: JSON.stringify({
                     //inputRef.current.value
-                    email: testData
+                    'input_email': testData
                 }),
 
                 headers: {
@@ -31,8 +29,8 @@ export default function ReserveInput() {
                 <Input className="w-6/12" type="text" label={'Your Last Name'} />
             </div>
             <Input className="w-full mt-10" type="email" label={'Your Email'} />
-            <Input className="w-full mt-10" type="text" label={'Amount of Guests'} />
-            <Button color='secondary' className=' w-full text-2xl my-14 font-roboto font-light bg-black py-7 rounded-lg'>
+            <Input className="w-full mt-10" type="number" label={'Amount of Guests'} />
+            <Button color='secondary' className=' w-full text-2xl my-14 font-roboto font-light bg-black py-7 rounded-lg' onClick={subscribeUser}>
                 RESERVE
             </Button>
         </div>
