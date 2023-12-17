b2034d1f8e252079c4c90ea7eff0e6b93332fec0
Sally Chik <sc399@njit.edu>
2023-12-16 20:49:07 -0500

    Added values for input fields and transaction api

--- is218final/app/components/ReserveInput.tsx
+++ is218final/app/components/ReserveInput.tsx
@@ -1,35 +1,58 @@
 'use client'
 import { Button, Input } from '@nextui-org/react'
-import { useRef } from 'react'
+import { ChangeEvent, useState } from 'react'
+
+const mailchimpFactory = require("@mailchimp/mailchimp_transactional/src/index.js");
+const mailchimp = mailchimpFactory(process.env.MANDRIL_API_KEY);
+
 
 export default function ReserveInput() {
-    const testData = 'example@gmail.com'
-    const inputRef = useRef(null)
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        guestAmount: ''
+    });
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name , value } = e.currentTarget;
+        setFormData({
+            ...formData, [name]: value
+        });
+    }
     const subscribeUser = async (e: any) => {
         e.preventDefault()
+        let response = 'default'
+        try {
+            // const response = await mailchimp.messages.send({
+            //     message
+            //   });
+        }
+        catch (e) {
+            let response = e
+        }
 
-        const res = fetch('/api',
-            {
-                body: JSON.stringify({
-                    //inputRef.current.value
-                    'input_email': testData
-                }),
+        // const res = fetch('/api',
+        //     {
+        //         body: JSON.stringify({
+        //             //inputRef.current.value
+        //             'input_email': testData
+        //         }),
 
-                headers: {
-                    'Content-Type': 'application/json',
-                },
+        //         headers: {
+        //             'Content-Type': 'application/json',
+        //         },
 
-                method: 'POST',
-            });
+        //         method: 'POST',
+        //     });
     };
     return (
         <div className="w-2/4 mt-10">
             <div className="flex flex-row gap-3">
-                <Input className="w-6/12" type="text" label={'Your First Name'} />
-                <Input className="w-6/12" type="text" label={'Your Last Name'} />
+                <Input id='firstName' className="w-6/12" type="text" onChange={handleInputChange} label={'Your First Name'} />
+                <Input id='firstName' className="w-6/12" type="text" onChange={handleInputChange} label={'Your Last Name'} />
             </div>
-            <Input className="w-full mt-10" type="email" label={'Your Email'} />
-            <Input className="w-full mt-10" type="number" label={'Amount of Guests'} />
+            <Input id='firstName' className="w-full mt-10" type="email" onChange={handleInputChange} label={'Your Email'} />
+            <Input id='firstName' className="w-full mt-10" type="number" onChange={handleInputChange} label={'Amount of Guests'} />
             <Button color='secondary' className=' w-full text-2xl my-14 font-roboto font-light bg-black py-7 rounded-lg' onClick={subscribeUser}>
                 RESERVE
             </Button>
