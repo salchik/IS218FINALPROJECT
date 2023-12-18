9ab09de5a594abf10e78e0134c5dcff23067127b
Sally Chik <sc399@njit.edu>
2023-12-17 22:43:32 -0500

    Added image to menu page

--- is218final/app/menu/page.tsx
+++ is218final/app/menu/page.tsx
@@ -3,12 +3,12 @@ import RestNav from "../components/RestNav";
 import RestFooter from "../components/RestFooter";
 
 export default function Menu() {
-    return (
-      <div className="min-w-screen min-h-screen">
-        <RestNav/>
-        <div className="relative w-full">
+  return (
+    <div className="min-w-screen min-h-screen">
+      <RestNav />
+      <div className="relative w-full">
         <div className='align-middle justify-center' style={{
-          backgroundImage: `url("/aboutImage.svg")`,
+          backgroundImage: `url("/menuImage.svg")`,
           backgroundSize: 'cover',
           backgroundPositionY: '70%',
           paddingBottom: '700px',
@@ -27,41 +27,41 @@ export default function Menu() {
             KINMEDAI, <span className=" font-light">Shi so, Trout Roe</span>
           </h4>
           <h4 className='font-roboto, font-bold'>
-          ARTICHOKE, <span className=" font-light">Shitake, Truffle</span>
+            ARTICHOKE, <span className=" font-light">Shitake, Truffle</span>
           </h4>
           <h4 className='font-roboto, font-bold'>
-          CUTTLEFISH, <span className=" font-light">Leche de Tigre, Quinoa</span>
+            CUTTLEFISH, <span className=" font-light">Leche de Tigre, Quinoa</span>
           </h4>
           <h4 className='font-roboto, font-bold'>
-          MILK BREAD, <span className=" font-light">Smoked Tofu, Fonio</span>
+            MILK BREAD, <span className=" font-light">Smoked Tofu, Fonio</span>
           </h4>
           <h4 className='font-roboto, font-bold'>
-          SCALLOP, <span className=" font-light">Saffron, Amaretto</span>
+            SCALLOP, <span className=" font-light">Saffron, Amaretto</span>
           </h4>
           <h4 className='font-roboto, font-bold'>
-           CARROT, <span className=" font-light">Almond, Satsuma
-           <br />
-           <br /> 
-           OR </span>
-           <br />
-           <br />
-           WHITE TRUFFLE, <span className=" font-light">Ravioli, Potato, Albufera </span> +55
+            CARROT, <span className=" font-light">Almond, Satsuma
+              <br />
+              <br />
+              OR </span>
+            <br />
+            <br />
+            WHITE TRUFFLE, <span className=" font-light">Ravioli, Potato, Albufera </span> +55
           </h4>
           <h4 className='font-roboto, font-bold'>
-          MADAI, <span className=" font-light">Lentil, Soy</span>
+            MADAI, <span className=" font-light">Lentil, Soy</span>
           </h4>
           <h4 className='font-roboto, font-bold'>
-          IBERICO PORK, <span className=" font-light">Eggplant, Plum, Chanterelles</span>
+            IBERICO PORK, <span className=" font-light">Eggplant, Plum, Chanterelles</span>
           </h4>
           <h4 className='font-roboto, font-bold'>
-          36-MONTH COMTE CHEESE, <span className=" font-light">Honey, Truffle</span>
+            36-MONTH COMTE CHEESE, <span className=" font-light">Honey, Truffle</span>
           </h4>
           <h4 className='font-roboto, font-bold'>
-          CHOCOLATE, <span className=" font-light">Wasabi, Porcini</span>
+            CHOCOLATE, <span className=" font-light">Wasabi, Porcini</span>
           </h4>
         </div>
       </div>
-        <RestFooter/>
-      </div>
-    )
-  }
+      <RestFooter />
+    </div>
+  )
+}
